01e8a938909d68a9ff3cac04bedd26caba6d28ea
james you <james.you01@sap.com>
2023-06-20 18:14:49 +0800

    cost function

---
+++ week1/cost_function.py
@@ -0,0 +1,40 @@
+import numpy as np
+%matplotlib widget
+import matplotlib.pyplot as plt
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+
+x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
+y_train = np.array([250, 300, 480,  430,   630, 730,])
+
+plt.close('all') 
+fig, ax, dyn_items = plt_stationary(x_train, y_train)
+updater = plt_update_onclick(fig, ax, x_train, y_train, dyn_items)
+
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    计算线性回归的成本函数。
+    
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+        total_cost(float)：使用 w，b 作为线性回归参数的成本
+               以拟合 x 和 y 中的数据点
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum = cost_sum + cost  
+    total_cost = (1 / (2 * m)) * cost_sum  
+
+    return total_cost
